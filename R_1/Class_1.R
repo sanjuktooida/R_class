5d5f727bf2a78f7b144017374f032775040e4be8
sanjuktooida <116233591+sanjuktooida@users.noreply.github.com>
2023-09-11 16:32:56 +0900

    Add files via upload

---
+++ R_1/Class_1.R
@@ -0,0 +1,98 @@
+#### 1강 ####
+a = 3
+a
+print(a)
+print("a")
+a <- 3
+b <<- 4
+a + b
+print(a+b)
+a - b
+3 - 2
+print(a - b)
+print(a * b)
+print(a/b)
+print(a^b)
+print(a%%b)
+print(a%/%b)
+
+print(a==b)
+
+print(!a*b)
+print(!a > b)
+print(a>b | b>a)
+print(a>b & b>a)
+TRUE
+a = TRUE
+b = "TTT"
+
+integer_var <- 5
+numeric_Var <- 3.14
+complex_var <- 2 + 3i
+character_var <- "Hello, world!"
+logical_var <- TRUE
+another_logical_Var <- FALSE
+NULL_var <- NULL
+NA_var <- NA
+negative_inf <- -Inf
+positive_inf <- Inf
+nan <- 0/0
+
+typeof(integer_var)
+typeof(numeric_Var)
+typeof(complex_var)
+typeof(character_var)
+typeof(logical_var)
+typeof(another_logical_Var)
+typeof(NULL_var)
+typeof(NA_var)
+typeof(negative_inf)
+typeof(positive_inf)
+typeof(nan)
+
+mode(integer_var)
+mode(numeric_Var)
+mode(complex_var)
+mode(character_var)
+mode(logical_var)
+mode(another_logical_Var)
+mode(NULL_var)
+mode(NA_var)
+mode(negative_inf)
+mode(positive_inf)
+mode(nan)
+
+sin(3)
+cos(3)
+tan(3)
+sinh(3)
+exp(2)
+log(3)
+log(3, base = 4)
+loglp(3)
+sqrt(3)
+abs(3)
+round(3, 4)
+
+a = c(1, 2, 3, 4, 5, 6)
+sum(c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+seq(1, 5, by = 2)
+seq(0, 1, length.out =1000) #반복사용함수
+rep(c(1, 2, 3), times = 2)
+rep(c(1, 2, 3), each = 2)
+
+a <- 3
+print(a)
+print(3)
+print("Hello")
+
+paste(3, 4, 5, 6, 7)
+print(paste(3, 4, 5, 6, 7))
+
+paste0(3+4, 4, 5, 6, 7)
+print(paste0(3+4, 4, 5, 6, 7))
+
+paste(3, 4, 5, 6, 7, sep = "-")
+paste0(3, 4, 5, 6, 7, sep = "-")
+
+??ggplot2
